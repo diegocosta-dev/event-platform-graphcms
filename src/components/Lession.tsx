537d9f4d72efc68846e9b11f340188923572a30a
diegocosta-dev <troiablender@gmail.com>
2022-06-30 13:51:35 -0300

    feat subscriber page

--- src/components/Lession.tsx
+++ src/components/Lession.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -11,10 +11,14 @@ interface LessonProps {
 }
 
 export function Lession (props: LessonProps) {
+  const { slug } = useParams<{slug: string}>()
+
   const isLessonAvaliable = isPast(props.avaliableAt)
   const avaliableDateFormatted = format(props.avaliableAt, "EEEE ' • ' d' de 'MMMM' • 'k'h'mm", {
     locale: ptBR
   })
+
+  const isActiveLesson = slug === props.slug
 
   return (
     isLessonAvaliable ? (
@@ -22,17 +26,17 @@ export function Lession (props: LessonProps) {
         <span className="text-gray-300">
           {avaliableDateFormatted}
         </span>
-        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${ isActiveLesson ? 'bg-green-500' : '' }`}>
           <header className="flex items-center justify-between">
-              <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <span className={`text-sm text-blue-500 font-medium flex items-center gap-2 ${isActiveLesson ? 'text-white' : 'text-blue-500'}`}>
                 <CheckCircle size={20} />
                 Conteúdo liberado
               </span>
-            <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+            <span className={`text-xs rounded px-2 py-[0.125rem] text-white border font-bold ${isActiveLesson ? 'border-white' : 'border-green-300 '}`}>
               {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
             </span>
           </header>
-          <strong className="text-gray-200 mt-5 block">
+          <strong className={`mt-5 block ${isActiveLesson ? 'text-white' : 'text-gray-200'}`}>
             {props.title}
           </strong>
         </div>
---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,103 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Logo } from "../components/Logo.svg";
+import { ReactIcon } from "../components/React.svg";
+
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber ($name: String!, $email: String!) {
+    createSubscriber(data: {name: $name, email: $email}) {
+      id
+    }
+  }
+`
+
+export function Subscribe () {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [createSubscriber, {loading, error}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+  const navigate = useNavigate()
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault()
+    
+    if(name && email) {
+      await createSubscriber({
+        variables: {
+          name,
+          email
+        }
+      })
+
+      navigate('/event')
+    }
+
+  }
+  
+  return (
+    <div className="relative min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <ReactIcon />
+        <div className="max-w-[640px]">
+          <Logo />
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Construa uma <strong className="text-blue-500">aplicação completa,</strong> do zero, com <strong className="text-blue-500">React</strong>
+          </h1>
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+          </p>
+        </div>
+
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Inscreva-se gratuitamente
+          </strong>
+          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+            <input 
+            className="bg-gray-900 rounded px-5 h-14"
+            type="text"
+            placeholder="Seu nome completo"
+            required
+            onChange={event => setName(event.target.value)}
+            />
+
+            <input 
+            className="bg-gray-900 rounded px-5 h-14"
+            type="email"
+            placeholder="Digite seu e-mail"
+            required
+            onChange={event => setEmail(event.target.value)}
+            />
+
+            <button
+            disabled={loading}
+            className="mt-4 disabled:bg-opacity-50 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+            type="submit"
+            >
+              garantir minha vaga
+            </button>
+          </form>
+          <div className="relative mt-4">
+            {error ? 
+              (
+                <div className="absolute w-full text-center text-sm p-2 top-0 font-bold text-white bg-orange-400">
+                  E-mail já cadastrado!
+                </div>
+              )
+              :
+              ''
+            }
+          </div>
+        </div>
+      </div>
+
+      <img 
+      className="mt-10"
+      src="/src/assets/code-mockup.png" 
+      alt=""
+      />
+    </div>
+  )
+}
